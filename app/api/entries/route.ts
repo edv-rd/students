9972eb383b4c20afe09198d14d2e18fdc990ec9c
Ed Code <edvard.ekstrom@gmail.com>
2023-09-10 22:53:15 +0200

    typing fix

--- app/api/entries/route.ts
+++ app/api/entries/route.ts
@@ -1,9 +1,7 @@
-import { NextApiRequest } from "next";
-
 import Entry from "@models/entries";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (request: NextApiRequest) => {
+export const GET = async (request: Request) => {
   try {
     await connectToDB();
 
@@ -11,6 +9,6 @@ export const GET = async (request: NextApiRequest) => {
 
     return new Response(JSON.stringify(entries), { status: 200 });
   } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 500 });
+    return new Response("Failed to fetch entries", { status: 500 });
   }
 };
