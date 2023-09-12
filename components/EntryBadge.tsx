60ce1c0ce6b099f35668b70e2b1d8bf99f5653c1
Ed Code <edvard.ekstrom@gmail.com>
2023-09-12 16:34:24 +0200

    small type fix again in EntryBadge :)

--- components/EntryBadge.tsx
+++ components/EntryBadge.tsx
@@ -4,14 +4,14 @@ import React from "react";
 
 declare interface AssignmentObject {
   entry: object;
-  _id: number;
+
   name?: string;
   instructions?: string;
 }
 
 function EntryBadge(assignment: AssignmentObject) {
   return (
-    <div key={assignment._id}>
+    <div>
       <p>{assignment.name}</p>
       <h2>{assignment.instructions}</h2>
     </div>
